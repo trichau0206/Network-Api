f022d1a037f082ffbed61ab750d1c5fdb3724091
trichau0206 <trichauphuoc@gmail.com>
2023-05-25 21:33:42 -0700

    fixed api routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,21 +1,20 @@
 const router = require("express").Router();
 const {
-  getAllUser,
-  getUserById,
-  createUser,
+  getUsers,
+  getSingleUser,
   updateUser,
+  createUser,
   deleteUser,
   addFriend,
-  removeFriend,
-} = require("../../controllers/user-controller");
+  deleteFriend,
+} = require("../../controllers/userController");
 
 // /api/users
-router.route("/").get(getAllUser).post(createUser);
+router.route("/").get(getUsers).post(createUser);
 
-// /api/users/:id
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/friends/:friendId
-router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
